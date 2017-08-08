b63ead85041515587fea320acfa15d16a48efe33
lovepurple-ourpalm <songguangze@outlook.com>
2017-08-08 19:39:53 +0800

    init commit

---
+++ EnterBeijing/EnterBeijing/common.py
@@ -0,0 +1,12 @@
+
+httpHeader = {
+    "Host":"api.jinjingzheng.zhongchebaolian.com",
+	"accept": "*/*",
+	"x-requested-with": "XMLHttpRequest",
+	"accept-language":"zh-cn",
+	"content-type":"application/x-www-form-urlencoded",
+	"origin": "https://api.jinjingzheng.zhongchebaolian.com",
+	"user-agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89",
+	"referer":"https://api.jinjingzheng.zhongchebaolian.com/enterbj/jsp/enterbj/index.jsp",
+	"cookie": "JSESSIONID=9E56E86F02184BF5E1D8BC9C05C5D76C",
+}
